a894bbf73c8927caa86c1ba3d2aef73dc567408f
cancer <zodiac.cancer.j6@gmail.com>
2020-08-02 16:40:38 +0900

    improve environments

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,21 @@ import svelte from 'rollup-plugin-svelte';
 import autoPreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import html from '@rollup/plugin-html';
-import serve from 'rollup-plugin-serve'
-import resolve from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+import resolve, { DEFAULTS } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dotenv from 'dotenv';
+import replace from '@rollup/plugin-replace';
+
+dotenv.config();
+
+if (typeof process.env.AUTH0_DOMAIN === 'undefined') {
+  throw new Error('Missing AUTH0_DOMAIN in env.');
+}
+
+if (typeof process.env.AUTH0_CLIENT_ID === 'undefined') {
+  throw new Error('Missing AUTH0_CLIENT_ID in env.');
+}
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -15,23 +27,46 @@ const config = {
     file: 'dist/bundle.js',
     format: 'iife',
     name: 'app',
-    plugins: [html()]
+    plugins: [
+      html({
+        publicPath: '/',
+      }),
+    ],
   },
   plugins: [
+    replace({
+      'process.env.AUTH0_DOMAIN': JSON.stringify(process.env.AUTH0_DOMAIN),
+      'process.env.AUTH0_CLIENT_ID': JSON.stringify(
+        process.env.AUTH0_CLIENT_ID
+      ),
+    }),
     svelte({
       dev: !production,
-      css: css => {
+      css: (css) => {
         css.write('dist/bundle.css');
       },
-      preprocess: autoPreprocess()
+      preprocess: autoPreprocess({
+        defaults: {
+          script: 'typescript',
+          css: 'scss',
+        },
+      }),
     }),
     resolve({
       browser: true,
       dedupe: ['svelte'],
+      extensions: [...DEFAULTS.extensions, '.svelte'],
     }),
     commonjs(),
     typescript({ sourceMap: !production }),
-    !production && serve('dist'),
+    !production &&
+      serve({
+        contentBase: 'dist',
+        historyApiFallback: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }),
   ],
 };
 
